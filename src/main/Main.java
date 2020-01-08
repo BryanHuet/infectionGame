843da59817540ddbdc33b7dc051f3d43c842f7b5
unknown <bryan050@sfr.fr>
2020-01-08 20:55:32 +0100

    project model

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package src.main;
+import src.ressource.*;
+
+public class Main{
+  public static void main(String[] args){
+      Piont a = new Piont("white");
+      System.out.println(a.getColor());
+  }
+}
---
+++ src/ressource/Piont.java
@@ -0,0 +1,14 @@
+package src.ressource;
+
+public class Piont{
+  protected String color;
+
+  public Piont(String color){
+    this.color = color;
+  }
+
+  public String getColor(){
+    return this.color;
+  }
+
+}
