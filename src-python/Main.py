1e9c63aa6e53fd261f88ff50c5d0a7d01d660ca2
BryanHuet <huetb050@gmail.com>
2020-01-15 18:33:05 +0100

    u[

--- src-python/Main.py
+++ src-python/Main.py
@@ -18,22 +18,24 @@ etat.currentPlayer="j1"
 etat.board[0][0]="j1"
 etat.board[1][1]="j2"
 
+move=mv.Move((0,0),(1,0),0)
+etat=etat.play(move);
 affiche(etat.board)
 
 
 a=1
-while (not(etat.isFinished())):
-    p=etat.currentPlayer
-    l=etat.getMoves(p)
-    #m=choice(l)
-    m=best_move(etat,l)
-    etat=etat.play(m)
-    if (a%2==0):
-        etat.currentPlayer="j1"
-    else:
-        etat.currentPlayer="j2"
-    a+=1
-    affiche(etat.board)
+#while (not(etat.isFinished())):
+#    p=etat.currentPlayer
+#    l=etat.getMoves(p)
+#    m=choice(l)
+    #m=best_move(etat,l)
+#    etat=etat.play(m)
+#    if (a%2==0):
+#        etat.currentPlayer="j1"
+#    else:
+#        etat.currentPlayer="j2"
+#    a+=1
+    #affiche(etat.board)
 
-print("nbPionts j1: ",etat.nbPionts("j1"))
-print("nbPionts j2: ",etat.nbPionts("j2"))
+#print("nbPionts j1: ",etat.nbPionts("j1"))
+#print("nbPionts j2: ",etat.nbPionts("j2"))
